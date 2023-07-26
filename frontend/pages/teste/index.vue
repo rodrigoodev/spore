1f639bc47df9307049f3f9656df77b7d2c08f2c4
rodrigorgrb@gmail.com <rodrigorgrb@gmail.com>
2023-07-26 02:50:11 -0300

    feat: adding tree js

---
+++ frontend/pages/teste/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { TresCanvas } from "@tresjs/core";
+</script>
+
+<template>
+  <TresCanvas clear-color="#82DBC5" window-size>
+    <TresPerspectiveCamera :position="[3, 3, 3]" :look-at="[0, 0, 0]" />
+    <TresMesh>
+      <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
+      <TresMeshBasicMaterial color="orange" />
+    </TresMesh>
+    <TresAmbientLight :intensity="1" />
+  </TresCanvas>
+</template>
